567a6e10d42964cc2a1c07922d800d1106aa0cfe
enkhbold470 <enkhbold470@gmail.com>
2025-02-15 12:57:25 -0800

    Remove WebcamDetect component from Home page

--- client-side/app/test/page.tsx
+++ client-side/app/test/page.tsx
@@ -1,10 +1,8 @@
-import WebcamDetect from "@/components/WebcamDetect";
 
 export default function Home() {
   return (
     <main>
       <h1>Live Webcam Object Detection with YOLO</h1>
-      <WebcamDetect />
     </main>
   );
 }
